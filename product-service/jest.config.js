3f4490d781461ceeac0c081b2e36b22661eb082e
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-04-06 15:41:55 +0300

    task3: add basic unit tests

---
+++ product-service/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@functions/(.*)': '<rootDir>/src/functions/$1',
+    '@libs/(.*)': '<rootDir>/src/libs/$1',
+  },
+}
