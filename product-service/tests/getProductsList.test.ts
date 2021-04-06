25341e6d034b0841dafbc6e3ce29422d3f420393
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-04-06 17:20:20 +0300

    task3: add basic unit tests

--- product-service/tests/getProductsList.test.ts
+++ product-service/tests/getProductsList.test.ts
@@ -1,55 +1,56 @@
 import { main } from '../src/functions/getProductsList/handler';
-// import { productsList } from '../src/mocks/products-list';
+import { productsList } from '../src/mocks/products-list';
 
 const mockContext = {
-  awsRequestId: "9e8a207c-1f99-4b9b-a413-02beb6250895",
-  invokeid: "id",
-  logGroupName: "/aws/lambda/product-service-dev-getProductsList",
-  logStreamName: "2015/09/22/[HEAD]13370a84ca4ed8b77c427af260",
-  functionVersion: "HEAD",
-  invokedFunctionArn: "123",
+  awsRequestId: '',
+  invokeid: '',
+  logGroupName: '',
+  logStreamName: '',
+  functionVersion: '',
+  invokedFunctionArn: '',
   isDefaultFunctionVersion: true,
-  functionName: "product-service-dev-getProductsList",
-  memoryLimitInMB: "1024",
+  functionName: '',
+  memoryLimitInMB: '',
   callbackWaitsForEmptyEventLoop: false,
   identity: {
-    cognitoIdentityId: "test-id",
-    cognitoIdentityPoolId: "test-id",
+    cognitoIdentityId: '',
+    cognitoIdentityPoolId: '',
   },
   clientContext: {
     client: {
-      installationId: "1",
-      appTitle: "1",
-      appVersionName: "1",
-      appVersionCode: "2",
-      appPackageName: "3",
+      installationId: '',
+      appTitle: '',
+      appVersionName: '',
+      appVersionCode: '',
+      appPackageName: '',
     },
     env: {
-      platformVersion: "1",
-      platform: "1",
-      make: "1",
-      model: "1",
-      locale: "1",
+      platformVersion: '',
+      platform: '',
+      make: '',
+      model: '',
+      locale: '',
     },
   },
-  getRemainingTimeInMillis: () => 1,
   done: () => {},
   fail: () => {},
   succeed: () => {},
+  getRemainingTimeInMillis: () => 1,
 };
 const mockEvent = {};
 const mockCallback = undefined;
 
-// const MOCK_RESULT = {
-//   statusCode: 200,
-//   headers: {
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Credentials": true,
-//   },
-//   body: productsList,
-// };
+describe('getProductList function', () => {
+  test('should return products list mock and 200 status code', async () => {
+    const mockResponse = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(productsList),
+    };
 
-test('correct greeting is generated', async () => {
-  console.log(await main(mockEvent, mockContext, mockCallback));
-  expect(true).toBe(true);
+    expect(await main(mockEvent, mockContext, mockCallback)).toEqual(mockResponse);
+  });
 });
