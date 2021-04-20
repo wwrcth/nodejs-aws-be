499cc1e5bc205b170dd0876cde59a25230375718
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-04-20 21:22:17 +0300

    task 4: add db usage in getProductById

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,25 +1,31 @@
 import 'source-map-support/register';
+import Joi from 'joi';
 
-import {
-  formatJSONResponse,
-  formatJSONError,
-  ValidatedEventAPIGatewayProxyEvent,
-} from '@libs/api-gateway';
+import { formatJSONResponse, formatJSONError } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
 import { GetProductByIdService } from '@services/get-product-by-id.service';
+import { DbConnectService } from '@services/db-connect.service';
 
-export class GetProductByIdHandler {
-  constructor(
-    private getProductByIdService: GetProductByIdService,
-  ) {}
+const getProductById = (getProductByIdService: GetProductByIdService) => async (event) => {
+  try {
+    const productIdValidationScheme = Joi.string().uuid().required();
+    const { productId } = event.pathParameters;
+    const { error } = productIdValidationScheme.validate(productId);
 
-  handle: ValidatedEventAPIGatewayProxyEvent<any> = async(event) => {
-    try {
-      const { productId } = event.pathParameters;
-      const product = await this.getProductByIdService.getProductById(productId);
-
-      return product ? formatJSONResponse(product) : formatJSONError({ message: 'Product not found' }, 404);
-    } catch (err) {
-      return formatJSONError({ message: err });
+    if (error) {
+      return formatJSONError({ message: 'Please pass product id in uuid format' }, 400);
     }
+
+    const product = await getProductByIdService.getProductById(productId);
+
+    return product ? formatJSONResponse(product) : formatJSONError({ message: 'Product not found' }, 404);
+  } catch (err) {
+    return formatJSONError({ message: err });
   }
 }
+
+const dbConnectService = new DbConnectService();
+const getProductByIdService = new GetProductByIdService(dbConnectService);
+
+export const main = middyfy({ handler: getProductById(getProductByIdService), dbConnectService });
+
