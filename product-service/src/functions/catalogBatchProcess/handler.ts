821147b4580c8ee2ffd4b4004ee4941fae7c8c71
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-05-22 05:21:33 +0300

    task 6: add sns management service

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,42 +1,23 @@
 import 'source-map-support/register';
-import AWS from 'aws-sdk';
 
-// import { formatJSONResponse, formatJSONError } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { DbConnectService } from '@services/db-connect.service';
-import {SQSHandler} from "aws-lambda";
+import { SnsManagementService } from '@services/sns-management.service';
+// import {ProductService} from "@services/product.service";
 // import { ProductService } from '@services/product.service';
 
-const catalogBatchProcess: SQSHandler = async (event) => {
-  try {
-    console.log('catalogBatchProcess');
-    console.log(event);
-    const sns = new AWS.SNS({ region: 'eu-west-1' });
-    for (const record of event.Records) {
-      const product = JSON.parse(record.body);
-      console.log('Message Body -->  ', product);
-      await sns.publish({
-        Subject: 'Import Products CSV info',
-        Message: record.body,
-        MessageAttributes: {
-          price: {
-            DataType: 'Number',
-            StringValue: product.price
-          }
-        },
-        TopicArn: process.env.SNS_ARN,
-      }).promise();
+const catalogBatchProcess = (snsManagementService: SnsManagementService) => async (event) => {
+  for (const { body } of event.Records) {
+    const { price } = JSON.parse(body);
 
-      // Do something
-    }
-
-    // return formatJSONResponse({ test: 'success message!' });
-  } catch (err) {
-    // return formatJSONError({ message: err });
+    console.log(`Message body: ${body}`);
+    await snsManagementService.publish(body, price);
+    console.log('Publishing to SNS is finished');
   }
 }
 
 const dbConnectService = new DbConnectService();
+const snsManagementService = new SnsManagementService();
 // const productService = new ProductService(dbConnectService);
 
-export const main = middyfy({ handler: catalogBatchProcess, dbConnectService });
+export const main = middyfy({ handler: catalogBatchProcess(snsManagementService), dbConnectService });
